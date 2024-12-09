036862faf2596d1a44361b0ddf7d07bf2899d5fa
David Dantas <d6v1ddantas@gmail.com>
2024-12-09 15:21:29 -0300

    Removendo Sobre e contato

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def home(requests):
     return render(requests,template_name='recipes/home.html',context={'name':'David Dantas'})
-def sobre(requests):
-    return HttpResponse('Sobre')
-
-def contato(requests):
-    return render(requests,template_name='base.html')
