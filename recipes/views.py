c547b60dbcc63a5f5de820d055e6a26b4e954bc0
David Dantas <d6v1ddantas@gmail.com>
2024-12-22 18:28:11 -0300

    finalizando modulo  Django Models e ORM (Object Relational Mapper)

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_list_or_404,get_object_or_404
 from utils.recipe.functio import make_recipe
+from recipes.models import Recipe
 
 def home(requests):
-    #return render(requests,template_name='recipes/pages/home.html',context={'recipes':[make_recipe() for _ in range(10)]})
-    return render(request=requests, template_name='recipes/pages/home.html', context={'recipes': [make_recipe()
-                                                                                                  for _ in range(10)]})
+    recipes = Recipe.objects.filter(is_published=True)
+    return render(request=requests, template_name='recipes/pages/home.html', context={'recipes':recipes})
+def category(requests,category_id):
+   # recipes = Recipe.objects.filter(category__id = category_id,is_published=True)
+    recipes = get_list_or_404( Recipe.objects.filter(category__id = category_id,is_published=True).order_by('-id'))
+    return render(request=requests, template_name='recipes/pages/category.html', context={'recipes':recipes,'title':f'{ recipes[0].category.name} - Category|'})
 
 def recipe(requests,id):
-    return render(requests,template_name='recipes/pages/recipe-view.html',context={'recipe':make_recipe(),'is_detail_page':True})
+    #recipe = Recipe.objects.filter(id=id).first()
+    recipe = get_object_or_404(Recipe,pk=id,is_published=True)
+    return render(requests,template_name='recipes/pages/recipe-view.html',context={'recipe':recipe,'is_detail_page':True})
